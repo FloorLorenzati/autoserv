6087aa53fa1f38005bbadc5a2a564bfb22d33c90
FloorLorenzati <lorenzatiflorencia@gmail.com>
2024-04-08 17:27:11 -0300

    Ultimando detalles

--- src/pages/descripcion.js
+++ src/pages/descripcion.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../css/descripcion.css";
 import simple from "../img/simple.png";
+import bañado from "../img/bañado.png";
+
 import logo from "../img/logo.png";
 
 
@@ -20,13 +22,14 @@ function Descripcion() {
             return (
                 <>
                     <div className='fondoDescripcion'>
-                        <img src={logo} alt="Logo" className='logo m-5' style={{ maxHeight: '20vh' }} />
                         <div className="contenedorCentralMobile">
                             <h2 className='text-center'>SIMPLE</h2>
                             <br></br>
                             <img src={simple} alt="Imagen Grande" className="img-fluid imagenGrandeMobile " />
+                            <img src={bañado} alt="Imagen Grande" className="img-fluid imagenGrandeMobile " />
+
                             <p className='fs-3'> Cucurucho con una bocha de helado, sabor a elección.</p>
-                            
+
                             <p className='fs-3'><p className='fs-3'> Cucurucho con baño de repostería y crocante de maní, con una bochas de helado, sabores a elección.</p>
                                 <br></br>
                                 <p className='fs-3'> Cucurucho con baño de repostería y crocante de maní, con una bochas de helado, sabores a elección.</p>
@@ -35,57 +38,53 @@ function Descripcion() {
                             <Link to="/pago" className="btn btnGreen btn-success btnComprar customBtn" role="button">
                                 Pagar
                             </Link>
+                            <br></br>
                             <Link to="/pantalla2" className="btn btnRed btn-danger btnComprar customBtn" role="button">
                                 Cancelar Orden
                             </Link>
                         </div>
 
                     </div>
-<br></br>
+                    <br></br>
                 </>
             );
         } else {
             return (
                 <>
                     <div className='fondoDescripcion'>
-                        <img src={logo} alt="Logo" className='logo m-5' style={{ maxHeight: '20vh' }} />
-
                         <div className="row">
                             <div className="col-md-2"></div>
                             <div className="col-md-8">
                                 <div className="contenedor-central">
-
                                     <div className="row">
                                         <div className="col-md-6">
                                             <h2 className='text-center'>SIMPLE</h2>
-                                            <br></br>
-
+                                            <br />
+                                            <br />
                                             <p className='fs-3'> Cucurucho con una bocha de helado, sabor a elección.</p>
-                                            <br></br>
-                                            <p className='fs-3'><p className='fs-3'> Cucurucho con baño de repostería y crocante de maní, con una bochas de helado, sabores a elección.</p>
-                                                <br></br>
-                                                <p className='fs-3'> Cucurucho con baño de repostería y crocante de maní, con una bochas de helado, sabores a elección.</p>
-                                            </p>
+                                            <br /><br />
+                                            <p className='fs-3'> Cucurucho con baño de repostería y crocante de maní, con una bocha de helado, sabores a elección.</p>
+                                            <br /><br />
+                                            <p className='fs-3'> Cucurucho con baño de repostería y crocante de maní, con una bocha de helado, sabores a elección.</p>
                                         </div>
                                         <div className="col-md-6">
-                                            <img src={simple} alt="Imagen Grande" className="img-fluid imagen-grande" />
-
-                                            <Link to="/pago" className="btn btnGreen btn-success btnComprar customBtn" role="button">
-                                                Pagar
-                                            </Link>
-                                            <Link to="/pantalla2" className="btn btnRed btn-danger btnComprar customBtn" role="button">
-                                                Cancelar Orden
-                                            </Link>
-
+                                            <img src={simple} alt="Imagen Grande" className="img-fluid imagen-pequena" />
+                                            <img src={bañado} alt="Otra Imagen" className="img-fluid imagen-derecha" />
                                         </div>
-                                        <br></br>
                                         <h1>$12.89</h1>
+                                        <Link to="/pago" className="btn btnGreen btn-success btnComprar customBtn" role="button">
+                                            Pagar
+                                        </Link>
+                                        <Link to="/pantalla2" className="btn btnRed btn-danger btnComprar customBtn" role="button">
+                                            Cancelar Orden
+                                        </Link>
                                     </div>
+
                                 </div>
                             </div>
-
                         </div>
                     </div>
+
 
                 </>
             );
@@ -98,7 +97,7 @@ function Descripcion() {
         <>
 
             <div className='portadaPago'>
-
+            <img src={logo} alt="Logo" className='logo m-5' style={{ maxHeight: '20vh' }} />
 
                 {menorA1000pxContenedor()}
 
